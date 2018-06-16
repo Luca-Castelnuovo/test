93116fd78e20767c9d42f7e0fb680cf32b7bc701
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-16 19:20:34 +0200

    d

--- functions.php
+++ functions.php
@@ -175,7 +175,7 @@ function my_project($project_id)
         echo '<ul>';
         while ($row = $result->fetch_assoc()) {
             $file_name  = $row["file_name"];
-            echo '<li><a href="/users/' . $_SESSION['user_name'] . '/' . $project_name . '' . $file_name . '">' . $file_name . '</a></li>';
+            echo '<li><a href="/users/' . $_SESSION['user_name'] . '/' . $project_name . $file_name . '">' . $file_name . '</a></li>';
         }
         echo '</ul>';
     } else {
