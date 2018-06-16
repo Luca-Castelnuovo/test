d488d72719528cc799a6e285680442a1f0bdd65a
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-16 11:27:21 +0200

    d

--- authentication/index.php
+++ authentication/index.php
@@ -4,23 +4,10 @@
 
 <head>
     <meta charset="utf-8">
-    <meta content="ie=edge" http-equiv="x-ua-compatible">
     <meta content="width=device-width,initial-scale=1,shrink-to-fit=no" name="viewport">
-    <meta content="Luca Castelnuovo" name="author">
-    <meta content="Luca Castelnuovo is a 15 years old developer from The Netherlands" name="description">
-    <meta content="Luca, Castelnuovo, young, developer, the netherlands, soest, lucacastelnuovo, techassistants soest, webdevelopment, Luca Castelnuovo, tech assistants, techassistant, tasoest, betasterren, and aannemersbedrijf" name="keywords">
-    <meta content="summary" name="twitter:card">
-    <meta content="@LucaCastelnuovo" name="twitter:site">
-    <meta content="@LucaCastelnuovo" property="twitter:creator">
-    <meta content="https://lucacastelnuovo.nl" property="og:url">
-    <meta content="Luca Castelnuovo" property="og:title">
-    <meta content="Luca Castelnuovo is a 15 years old developer from The Netherlands." property="og:description">
-    <meta content="https://lucacastelnuovo.nl/images/favicon.ico" property="og:image">
-    <meta content="website" property="og:type">
-    <meta content="Luca Castelnuovo" property="og:site_name">
-    <link href="https://lucacastelnuovo.nl" rel="canonical">
-    <title>Test - Luca Castelnuovo</title>
-    <meta content="origin-when-crossorigin" name="referrer">
+
+    <link href=https://lucacastelnuovo.nl/images/favicon.ico rel="shortcut icon">
+    <title>Login - test.lucacastelnuovo.nl</title>
 
     <link as="style" href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" onload='this.rel="stylesheet"' rel="preload">
     <link as="style" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" onload='this.rel="stylesheet"' rel="preload">
