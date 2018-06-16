4ae9ae8b4d678a2ca14d1d8fc1f374fa6cf2a2af
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-16 16:59:32 +0200

    d

--- index.php
+++ index.php
@@ -26,6 +26,7 @@
     </div>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
     <script src="js/main.js"></script>
+    <script src="sweetalert2.all.min.js"></script>
 </body>
 
 </html>
