cc1265eaa9ca6531dd737691f723fa492ceeb8b7
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-16 16:17:16 +0200

    d

--- auth.php
+++ auth.php
@@ -11,21 +11,22 @@ $result = $mysqli->query("SELECT * FROM users WHERE user_name='$user_name'");
 if ($result->num_rows != 0) {
     $user = $result->fetch_assoc();
     if (password_verify($user_password, $user['user_password'])) {
-        $_SESSION['ip'] = ip() = $ip;
-        $_SESSION['user_name'] = $user['user_name'] = $username;
+        $_SESSION['ip'] = ip();
+        $_SESSION['user_name'] = $user['user_name'];
         $_SESSION['user_active'] = $user['user_active'];
         $_SESSION['user_type'] = $user['user_type'];
         $_SESSION['logged_in'] = true;
 
-        $text = date('Y-m-d H:i:s') . '	:	' . $ip . '	:	' . $username . PHP_EOL;
-        $file = fopen("ip-login.txt", "a+");
-        fwrite($file, $text);
+//        $text = date('Y-m-d H:i:s') . '	:	' . $_SESSION['ip'] . '	:	' . $_SESSION['user_name'] . PHP_EOL;
+//        $file = fopen("ip-login.txt", "a+");
+//        fwrite($file, $text);
+
         $success = true;
     }
 }
 
 if ($success) {
-    $out = ["status" => true, "username" => $username];
+    $out = ["status" => true, "username" => $_SESSION['user_name']];
     echo json_encode($out);
 } else {
     $out = ["status" => false];
