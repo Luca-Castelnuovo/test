27ff99b585aea31882f2acb7552d9701446e04e4
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-16 10:36:24 +0200

    d

--- authentication/index.php
+++ authentication/index.php
@@ -1,49 +1,41 @@
-<?php
-require_once($_SERVER['DOCUMENT_ROOT'] . "/authentication/functions.php");
+<!DOCTYPE html>
 
-if (isset($_POST['submit'])) {
-    echo 'test';
-}
+<html lang="en">
 
-?>
-    <!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <meta content="ie=edge" http-equiv="x-ua-compatible">
+    <meta content="width=device-width,initial-scale=1,shrink-to-fit=no" name="viewport">
+    <meta content="Luca Castelnuovo" name="author">
+    <meta content="Luca Castelnuovo is a 15 years old developer from The Netherlands" name="description">
+    <meta content="Luca, Castelnuovo, young, developer, the netherlands, soest, lucacastelnuovo, techassistants soest, webdevelopment, Luca Castelnuovo, tech assistants, techassistant, tasoest, betasterren, and aannemersbedrijf" name="keywords">
+    <meta content="summary" name="twitter:card">
+    <meta content="@LucaCastelnuovo" name="twitter:site">
+    <meta content="@LucaCastelnuovo" property="twitter:creator">
+    <meta content="https://lucacastelnuovo.nl" property="og:url">
+    <meta content="Luca Castelnuovo" property="og:title">
+    <meta content="Luca Castelnuovo is a 15 years old developer from The Netherlands." property="og:description">
+    <meta content="https://lucacastelnuovo.nl/images/favicon.ico" property="og:image">
+    <meta content="website" property="og:type">
+    <meta content="Luca Castelnuovo" property="og:site_name">
+    <link href="https://lucacastelnuovo.nl" rel="canonical">
+    <title>Test - Luca Castelnuovo</title>
+    <meta content="origin-when-crossorigin" name="referrer">
 
-    <html lang="en">
+    <link as="style" href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" onload='this.rel="stylesheet"' rel="preload">
+    <link as="style" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" onload='this.rel="stylesheet"' rel="preload">
+    <link as="style" href="css/style.min.css" onload='this.rel="stylesheet"' rel="preload">
+</head>
 
-    <head>
-        <meta charset="utf-8">
-        <meta content="ie=edge" http-equiv="x-ua-compatible">
-        <meta content="width=device-width,initial-scale=1,shrink-to-fit=no" name="viewport">
-        <meta content="Luca Castelnuovo" name="author">
-        <meta content="Luca Castelnuovo is a 15 years old developer from The Netherlands" name="description">
-        <meta content="Luca, Castelnuovo, young, developer, the netherlands, soest, lucacastelnuovo, techassistants soest, webdevelopment, Luca Castelnuovo, tech assistants, techassistant, tasoest, betasterren, and aannemersbedrijf" name="keywords">
-        <meta content="summary" name="twitter:card">
-        <meta content="@LucaCastelnuovo" name="twitter:site">
-        <meta content="@LucaCastelnuovo" property="twitter:creator">
-        <meta content="https://lucacastelnuovo.nl" property="og:url">
-        <meta content="Luca Castelnuovo" property="og:title">
-        <meta content="Luca Castelnuovo is a 15 years old developer from The Netherlands." property="og:description">
-        <meta content="https://lucacastelnuovo.nl/images/favicon.ico" property="og:image">
-        <meta content="website" property="og:type">
-        <meta content="Luca Castelnuovo" property="og:site_name">
-        <link href="https://lucacastelnuovo.nl" rel="canonical">
-        <title>Test - Luca Castelnuovo</title>
-        <meta content="origin-when-crossorigin" name="referrer">
+<body>
+    <div class="wrapper">
+        <form class="login" method="post" action="auth.php">
+            <p class="title">Log in</p>
+            <input placeholder="Username" type="text" name="username" autocomplete="off" required="required" autofocus> <i class="fa fa-user"></i> <input placeholder="Password" type="password" name="password" autocomplete="off" required="required"> <i class="fa fa-key"></i> <button name="submit"><i class="spinner"></i> <span class="state">Log in</span></button>
+        </form>
+    </div>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
 
-        <link as="style" href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" onload='this.rel="stylesheet"' rel="preload">
-        <link as="style" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" onload='this.rel="stylesheet"' rel="preload">
-        <link as="style" href="css/style.min.css" onload='this.rel="stylesheet"' rel="preload">
-    </head>
-
-    <body>
-        <div class="wrapper">
-            <form class="login" method="post" action="auth.php">
-                <p class="title">Log in</p>
-                <input placeholder="Username" type="text" name="username" autocomplete="off" required="required" autofocus> <i class="fa fa-user"></i> <input placeholder="Password" type="password" name="password" autocomplete="off" required="required"> <i class="fa fa-key"></i> <button name="submit"><i class="spinner"></i> <span class="state">Log in</span></button>
-            </form>
-        </div>
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-        <script src="js/main.js"></script>
-    </body>
-
-    </html>
+</html>
