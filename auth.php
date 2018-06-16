4e87fc3ebbf51839562f6fbfe51d0feb4bd5d8a3
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-16 16:11:55 +0200

    d

--- auth.php
+++ auth.php
@@ -9,7 +9,7 @@ $password = clean_data($_GET['password']);
 $result = $mysqli->query("SELECT * FROM users WHERE username='$username'");
 
 if ($result->num_rows != 0) {
-    if (password_verify($_POST['password'], $user['password'])) {
+    if (password_verify($password, $user['password'])) {
         $ip = $_SESSION['ip'] = ip();
         $username = $_SESSION['user_name'] = $user['username'];
         $_SESSION['active'] = $user['active'];
