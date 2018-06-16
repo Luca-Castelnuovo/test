18a1670fc93d882c04f8c242de14ecf402da9499
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-16 18:19:35 +0200

    d

--- users/home.php
+++ users/home.php
@@ -2,8 +2,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
+    <meta content="width=device-width,initial-scale=1,shrink-to-fit=no" name="viewport">
+
+    <link href=https://lucacastelnuovo.nl/images/favicon.ico rel="shortcut icon">
     <title><?= $_SESSION['user_name'] ?></title>
+
+    <link as="style" href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" onload='this.rel="stylesheet"' rel="preload">
+    <link as="style" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" onload='this.rel="stylesheet"' rel="preload">
+    <link as="style" href="css/style.css" onload='this.rel="stylesheet"' rel="preload">
 </head>
 <body>
     <a href="/?logout"><h1><?= $_SESSION['user_name'] ?> user</h1></a>
