5882b0516a91061165ad8a410a358cc0b7a7f654
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-16 16:01:54 +0200

    d

---
+++ users/test/template.php
@@ -0,0 +1,14 @@
+<?php
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/functions.php");
+login('test');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Test</title>
+</head>
+<body>
+    <h1>test user</h1>
+</body>
+</html>
