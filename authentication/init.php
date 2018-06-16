fce29a63c0ac3fb8ee64f026e58e61ea9ac3a7cc
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-16 09:44:54 +0200

    d

---
+++ authentication/init.php
@@ -0,0 +1,6 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . "/authentication/classes/Database.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/authentication/classes/User.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/authentication/classes/Sessions.php");
+
+require_once($_SERVER['DOCUMENT_ROOT'] . "/authentication/functions.php");
