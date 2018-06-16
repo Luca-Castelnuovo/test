91a9eafae0da9764978eb2f7c161cb0a3e223d27
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-16 11:06:20 +0200

    d

--- authentication/index.php
+++ authentication/index.php
@@ -31,7 +31,8 @@
     <div class="wrapper">
         <form class="login" method="post" action="auth.php">
             <p class="title">Log in</p>
-            <input placeholder="Username" type="text" name="username" autocomplete="off" required="required" autofocus> <i class="fa fa-user"></i> <input placeholder="Password" type="password" name="password" autocomplete="off" required="required"> <i class="fa fa-key"></i> <button name="submit"><i class="spinner"></i> <span class="state">Log in</span></button>
+            <input placeholder="Username" type="text" name="username" autocomplete="off" required="required" class="text" autofocus> <i class="fa fa-user"></i> <input placeholder="Password" type="password" name="password" autocomplete="off" required="required" class="text"> <i class="fa fa-key"></i> <button name="submit"><i class="spinner"></i> <span class="state">Log in</span></button>
+            <div class="loading"><p>loading</p></div>
         </form>
     </div>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
