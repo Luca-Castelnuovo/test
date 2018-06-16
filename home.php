78eab0e179471868c5a839ab4628704e8f9155b4
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-16 19:16:16 +0200

    d

--- home.php
+++ home.php
@@ -18,7 +18,7 @@
 
 <body>
     <div class="wrapper">
-        <div class="login" style="padding-bottom: 0">
+        <div class="login" style="padding-bottom: 20px;">
             <h2>Your Projects:</h2>
             <?php if (isset($_GET['project'])) {
                 my_project($_GET['project']);
