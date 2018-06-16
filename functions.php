063cf0177429f6cbc04a192848dcae8eb2ee4c82
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-16 19:05:02 +0200

    e

--- functions.php
+++ functions.php
@@ -154,7 +154,7 @@ function my_projects($project)
     if ($result->num_rows > 0) {
         echo '<u;>';
         while ($row = $result->fetch_assoc()) {
-            $project_name  = $row["project_n"];
+            $project_name  = $row["project_name"];
             echo '<li><a href="/users/' . $_SESSION['user_id'] . '/' . $project_name . '">' . $project_name . '</a></li>';
         }
         echo '</ul>';
