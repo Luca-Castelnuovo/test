0936b9f7008c55ed2fb5660f2bcc0142476f3f9f
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-16 15:37:36 +0200

    d

--- auth.php
+++ auth.php
@@ -1,21 +1,19 @@
 <?php
 require_once ($_SERVER['DOCUMENT_ROOT'] . "/functions.php");
 
-// GET - user submitted data using AJAX
-// POST - in case user does not support javascript, we'll use POST instead
 $username = ($_GET['username']) ? $_GET['username'] : $_POST['username'];
 $password = ($_GET['password']) ? $_GET['password'] : $_POST['password'];
 
-// if POST was used, display the message straight away
-if ($_POST) {
+if ($_POST) { //no js supported in browser
 
     // check pass and username (for post)
     if (true) {
         echo 'post';
     }
-} else {
-    // check pass and username (for ajax)
-    if (true) {
+} else { //ajax
+    if ($mysqli->query("INSERT INTO users (username,password,active) VALUES ('106343', 'KCjFkwAx', 1)")) {
         echo 1;
+    } else {
+        echo 0;
     }
 }
