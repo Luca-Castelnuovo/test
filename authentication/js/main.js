5be243027b37bf01b1022104e0345f88e3691fd0
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-16 11:11:02 +0200

    d

--- authentication/js/main.js
+++ authentication/js/main.js
@@ -1,22 +1,22 @@
-//var working = false;
-//$('.login').on('submit', function(e) {
-//  e.preventDefault();
-//  if (working) return;
-//  working = true;
-//  var $this = $(this),
-//    $state = $this.find('button > .state');
-//  $this.addClass('loading');
-//  $state.html('Authenticating');
-//  setTimeout(function() {
-//    $this.addClass('ok');
-//    $state.html('Welcome back!');
-//    setTimeout(function() {
-//      $state.html('Log in');
-//      $this.removeClass('ok loading');
-//      working = false;
-//    }, 4000);
-//  }, 1000);
-//});
+var working = false;
+$('.login').on('submit', function (e) {
+    e.preventDefault();
+    if (working) return;
+    working = true;
+    var $this = $(this),
+        $state = $this.find('button > .state');
+    $this.addClass('loading');
+    $state.html('Authenticating');
+    setTimeout(function () {
+        $this.addClass('ok');
+        $state.html('Welcome back!');
+        setTimeout(function () {
+            $state.html('Log in');
+            $this.removeClass('ok loading');
+            working = false;
+        }, 4000);
+    }, 1000);
+});
 
 //if submit button is clicked
 $('#submit').click(function () {
@@ -43,8 +43,22 @@ $('#submit').click(function () {
     //disabled all the text fields
     $('.text').attr('disabled', 'true');
 
-    //show the loading sign
-    $('.loading').show();
+    e.preventDefault();
+    if (working) return;
+    working = true;
+    var $this = $(this),
+        $state = $this.find('button > .state');
+    $this.addClass('loading');
+    $state.html('Authenticating');
+    setTimeout(function () {
+        $this.addClass('ok');
+        $state.html('Welcome back!');
+        setTimeout(function () {
+            $state.html('Log in');
+            $this.removeClass('ok loading');
+            working = false;
+        }, 4000);
+    }, 1000);
 
     //start the ajax
     $.ajax({
