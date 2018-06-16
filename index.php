37f657def3d2def55137f36bffc422af1031fd52
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-16 11:42:19 +0200

    d

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home - test.lucacastelnuovo.nl</title>
+</head>
+<body>
+    <h1>home</h1>
+</body>
+</html>
