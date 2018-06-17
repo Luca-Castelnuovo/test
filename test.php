071077d0a8fa3801dc33ae9eaef2996a9f8448d7
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-17 21:39:55 +0200

    e

--- test.php
+++ test.php
@@ -1,7 +1,3 @@
-<?php require_once($_SERVER['DOCUMENT_ROOT'] . "/functions.php");
-if (isset($_GET['logout'])) {
-    logout('You are succesfully logged out!');
-} ?>
 <!DOCTYPE html>
 
 <html lang="en">
@@ -17,19 +13,14 @@ if (isset($_GET['logout'])) {
 </head>
 
 <body>
-<div class="wrapper">
-    <form class="login loading error">
-        <p class="title">Log in</p>
-        <input placeholder="Username" type="text" name="user_name" autocomplete="off" class="text" id="username"
-               autofocus> <i class="fa fa-user"></i>
-        <input placeholder="Password" type="password" name="user_password" autocomplete="off" class="text"
-               id="password"> <i class="fa fa-key"></i>
-        <input type="hidden" name="CSRFtoken" value="<?= csrf_gen(); ?>"/>
-        <button id="submit"><i class="spinner"></i> <span class="state">Log in</span></button>
-    </form>
-</div>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-<script src="js/login.js"></script>
+    <div class="wrapper">
+        <form class="login loading error">
+            <p class="title">Test</p>
+            <input placeholder="Username" type="text" name="user_name" autocomplete="off" class="text" id="username" autofocus> <i class="fa fa-user"></i>
+            <input placeholder="Password" type="password" name="user_password" autocomplete="off" class="text" id="password"> <i class="fa fa-key"></i>
+            <button id="submit"><i class="spinner"></i> <span class="state">Testing...</span></button>
+        </form>
+    </div>
 </body>
 
 </html>
