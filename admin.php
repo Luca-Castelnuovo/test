a7f8fffe2765752328fe7b3f608691ae9337d699
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-17 12:29:39 +0200

    e

--- admin.php
+++ admin.php
@@ -17,7 +17,7 @@
     <div class="wrapper">
         <form class="login" method="post" action="auth.php">
             <p class="title">Admin Panel</p>
-            <?php if (isset($_SESSION['invite_response'])) {echo $_SESSION['invite_response']; unset($_SESSION['invite_response']);} else {echo '<input type="hidden" name="CSRFtoken" value="' . csrf_gen() . '"/><button id="submit"><i class="spinner"></i> <span class="state">Generate Invite Code</span></button>';} ?>
+            <?php if (isset($_SESSION['invite_response'])) {echo $_SESSION['invite_response'];} else {echo '<input type="hidden" name="CSRFtoken" value="' . csrf_gen() . '"/><button id="submit"><i class="spinner"></i> <span class="state">Generate Invite Code</span></button>';} ?>
         </form>
     </div>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
