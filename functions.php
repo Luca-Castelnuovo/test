1c78d0506ebf65f45939a541f4ef1e8777b6a28a
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-17 14:22:56 +0200

    d

--- functions.php
+++ functions.php
@@ -175,7 +175,7 @@ function my_project($project_id)
             $file  = $row["file"];
             echo "<td class='inline'><a class='dropdown-trigger btn' href='?project={$file_id}' data-target='{$file_id}'>{$file}</a></td>";
             echo "<ul id='{$file_id}' class='dropdown-content'>
-                    <li><a href='users/{$_SESSION['user_name']}/{$file}'>view</a></li>
+                    <li><a href='users/{$_SESSION['user_name']}/{$file}' target='_blank'>view</a></li>
                     <li><a href='files?type=edit&id={$file_id}&project_id={$project_id}'>edit</a></li>
                     <li><a href='files?type=delete&id={$file_id}&project_id={$project_id}'>delete</a></li>
                 </ul>";
