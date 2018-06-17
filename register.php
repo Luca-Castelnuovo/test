d6657584c99e4a91d8f0df4b806ba545d3f1c8d8
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-17 09:48:34 +0200

    d

--- register.php
+++ register.php
@@ -3,14 +3,16 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/functions.php");
 
 if ($_SESSION['auth_code_valid'] && $_SESSION['auth_code_id'] === 1) {
     unset($_SESSION['auth_code_valid']);
+    $js = 'register.js'
     $title = 'Register';
     $button_text = 'Submit';
     $type = 'register';
     $content = ['<input placeholder="Username" type="text" name="user_name" autocomplete="off" class="text" id="username" autofocus> <i class="fa fa-user"></i>', '<input placeholder="Password" type="password" name="user_password" autocomplete="off" class="text" id="password"> <i class="fa fa-key"></i>', '<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>', '<script src="js/register.js"></script>'];
 } else {
+    $js = 'auth.js'
     $title = 'Invite Code';
     $button_text = 'Check Invite Code';
-    $content = ['<input placeholder="Invite Code" type="text" name="auth_code" autocomplete="off" class="text" autofocus value="' . $_GET['auth_code'] . '"> <i class="fa fa-barcode"></i>', '<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>', '<script src="js/auth.js"></script>'];
+    $content = ['<input placeholder="Invite Code" type="text" name="auth_code" autocomplete="off" class="text" autofocus value="' . $_GET['auth_code'] . '"> <i class="fa fa-barcode"></i>'];
 }
 
 ?>
@@ -39,6 +41,8 @@ if ($_SESSION['auth_code_valid'] && $_SESSION['auth_code_id'] === 1) {
             <button id="submit"><i class="spinner"></i> <span class="state"><?= $button_text ?></span></button>
         </form>
     </div>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <?php echo "<script src='js/{$js}'></script>" ?>
 </body>
 
 </html>
