c367b6d8053b8c7a28b27eca55d07d2f9d5d4d30
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-17 11:42:43 +0200

    d

--- admin.php
+++ admin.php
@@ -1,12 +1,13 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/functions.php"); login_admin(); ?>
 <!DOCTYPE html>
+
 <html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta content="width=device-width,initial-scale=1,shrink-to-fit=no" name="viewport">
     <link href=https://lucacastelnuovo.nl/images/favicon.ico rel="shortcut icon">
-    <title><?= $_SESSION['user_name'] ?></title>
+    <title>Admin Panel</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans:400,700">
@@ -14,14 +15,15 @@
 
 <body>
     <div class="wrapper">
-        <div class="login" style="padding-bottom: 20px;">
-            <?php if (isset($_GET['project'])) {my_project($_GET['project']);} else {my_projects();} ?>
-        </div>
+        <form class="login" method="post" action="auth.php">
+            <p class="title">Admin Panel</p>
+            <input placeholder="Email for invite" type="email" name="user_email" autocomplete="off" class="text" autofocus> <i class="fa fa-email"></i>
+            <input type="hidden" name="CSRFtoken" value="<?= csrf_gen(); ?>"/>
+            <button id="submit"><i class="spinner"></i> <span class="state">Send Invite</span></button>
+        </form>
     </div>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
-    <script>$('.dropdown-trigger').dropdown();</script>
+    <script src="js/invite.js"></script>
 </body>
 
 </html>
