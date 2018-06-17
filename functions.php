27fb831afc246cc5579ddf63b8446d0536f468ae
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-17 21:31:27 +0200

    e

--- functions.php
+++ functions.php
@@ -174,7 +174,7 @@ function my_project($project_id)
     $project_name = $projects['project_name'];
 
     $result = sql("SELECT * FROM files WHERE project_id='{$project_id}'AND owner_id='{$_SESSION['user_id']}'");
-    echo '<h2>Files:</h2><table><tr><td class="inline">';
+    echo '<h2>' . $project_name . ':</h2><table><tr><td class="inline">';
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
             $file_id = $row["id"];
