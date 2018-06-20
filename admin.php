65aaa119352b7dd70dca7ffda810ab7831d79479
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-20 09:20:22 +0200

    e

--- admin.php
+++ admin.php
@@ -17,6 +17,8 @@ if (isset($_SESSION['invite_response'])) {
 } elseif (isset($_GET['users'])) {
     $type = 'users';
     $result = sql("SELECT * FROM users");
+    $row = $result->fetch_assoc()
+    var_dump($row);
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
             $user_id = $row["id"];
