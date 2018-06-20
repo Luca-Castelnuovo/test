3ebd1346f7a27bbf2ff1c0067410dfbf8777f37b
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-20 08:25:03 +0200

    e

--- functions.php
+++ functions.php
@@ -102,10 +102,14 @@ function login_admin()
 
 function logout()
 {
-    if (isset($_COOKIE[session_name()])) {
-        setcookie(session_name(), “”, time() - 3600, “/”);
-    }
     $_SESSION = array();
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
     session_destroy();
     header('Location: /');
     exit;
