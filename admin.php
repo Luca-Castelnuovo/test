87e19fc73499e1253f80ad7aa6f881f9b88380a3
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-20 14:48:56 +0200

    e

--- admin.php
+++ admin.php
@@ -26,13 +26,10 @@ if (isset($_SESSION['invite_response'])) {
             $user_type = $row["user_type"];
             $user_active = $row["user_active"];
             if($user_active) {$user_status = 'Deactivate';} else {$user_status = 'Activate';}
-            array_push($content,"<a class='dropdown-trigger btn' href='#' data-target='{$user_id}'>{$user_name}</a>","<ul id='{$user_id}' class='dropdown-content'><li><a href='process?type=admin&admin_type=active&user_id={$user_id}&CSRFtoken={$csrftoken}'>{$user_status}</a></li><li><a href='?users&type=delete&id={$user_id}'>Delete</a></li></ul>");
+            array_push($content,"<a class='dropdown-trigger btn' href='#' data-target='{$user_id}'>{$user_name}</a>","<ul id='{$user_id}' class='dropdown-content'><li><a href='process?type=admin&admin_type=active&user_id={$user_id}&CSRFtoken={$csrftoken}'>{$user_status}</a></li><li><a href='process?type=admin&admin_type=delete&user_id={$user_id}&CSRFtoken={$csrftoken}''>Delete</a></li></ul>");
         }
     }
     $title = 'All Users';
-} elseif (isset($_GET['delete'])) {
-    array_push($content,'<input placeholder="To confirm type `delete` else type `no`" type="text" name="delete" autocomplete="off" class="text" autofocus> <i class="fa fa-trash"></i>', '<button id="submit"><span class="state">Delete User</span></button>');
-    $back_button = false;
 } else {
     $type = 'default';
     $content = ["<a class='dropdown-trigger btn' href='#' id='submit'>Generate Invite Code</a><a class='dropdown-trigger btn' href='?users'>Users</a><a class='dropdown-trigger btn' href='?login_log'>Login Log</a><a class='dropdown-trigger btn' href='/home'>Back</a>"];
@@ -56,7 +53,6 @@ if ($back_button) {
     <form class="login pd-20 <?php if($type == 'login_log') {echo 'admin log';}?>">
         <p class="title"><?= $title ?></p>
         <input type="hidden" name="CSRFtoken" value="<?= $csrftoken ?>"/>
-        <input type="hidden" name="type" value="<?= clean_data($_GET['type']) ?>" />
         <input type="hidden" name="id" value="<?= clean_data($_GET['id']) ?>" />
         <div class="inline">
             <?php foreach($content as $row) {echo $row;} ?>
@@ -64,7 +60,7 @@ if ($back_button) {
     </form>
 </div>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-<script src="js/admin.min.js"></script>
+<script src="js/invite.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
 <script>$('.dropdown-trigger').dropdown();</script>
--- js/admin_delete.js
+++ js/admin_delete.js
@@ -1,11 +1,10 @@
-$('#submit').click(function () {
+$('#submit_delete').click(function () {
 
     var CSRFtoken = $('input[name=CSRFtoken]');
     var user_id = $('input[name=id]');
     var user_delete = $('input[name=delete]');
-    var admin_type = $('input[name=type]');
 
-    var data = 'CSRFtoken=' + CSRFtoken.val() + '&type=admin&admin_type=' + admin_type.val() + 'user_id= ' + user_id.val() + 'user_delete=' + user_delete.val();
+    var data = 'CSRFtoken=' + CSRFtoken.val() + '&type=admin&admin_type=delete' + 'user_id= ' + user_id.val() + 'user_delete=' + user_delete.val();
 
     $.ajax({
         url: "process.php",
