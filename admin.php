ec793b12ecbb080550a2e69054e369495a568cef
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-20 09:23:27 +0200

    e

--- admin.php
+++ admin.php
@@ -6,40 +6,38 @@ $back_button = true;
 
 if (isset($_SESSION['invite_response'])) {
     $type = 'code_response';
-    $content = "<a class='dropdown-trigger btn' href='https://test.lucacastelnuovo.nl/register?auth_code={$_SESSION['invite_response']}'>Registration link</a>";
+    $content = ["<a class='dropdown-trigger btn' href='https://test.lucacastelnuovo.nl/register?auth_code={$_SESSION['invite_response']}'>Registration link</a>"];
     unset($_SESSION['invite_response']);
     $title = 'Registration Cod';
 } elseif (isset($_GET['login_log'])) {
     $type = 'login_log';
     $log_file_content = file_get_contents('login.txt');
-    $content = '<textarea rows="30" cols="60">' . $log_file_content . '</textarea><br>';
+    $content = ['<textarea rows="30" cols="60">' . $log_file_content . '</textarea><br>'];
     $title = 'Login Logs';
 } elseif (isset($_GET['users'])) {
     $type = 'users';
     $result = sql("SELECT * FROM users");
-    $row = $result->fetch_assoc();
-    var_dump($row);exit;
     if ($result->num_rows > 0) {
+        $content = [];
         while ($row = $result->fetch_assoc()) {
             $user_id = $row["id"];
             $user_name = $row["user_name"];
             $user_type = $row["user_type"];
             $user_active = $row["user_active"];
             if($user_active) {$user_status = 'Deactivate';} else {$user_status = 'Activate';}
-            $content = "<a class='dropdown-trigger btn' href='#' data-target='{$user_id}'>{$user_name}</a>";
-            $content = $content . "<ul id='{$user_id}' class='dropdown-content'><li><a href='?users&type=t_active&id={$user_id}'>{$user_status}</a></li><li><a href='?users&type=delete&id={$user_id}'>Delete</a></li></ul>";
+            array_push($content,"<a class='dropdown-trigger btn' href='#' data-target='{$user_id}'>{$user_name}</a>","<ul id='{$user_id}' class='dropdown-content'><li><a href='?users&type=t_active&id={$user_id}'>{$user_status}</a></li><li><a href='?users&type=delete&id={$user_id}'>Delete</a></li></ul>");
         }
     }
     $title = 'All Users';
 } else {
     $type = 'default';
-    $content = "<a class='dropdown-trigger btn' href='#' id='submit'>Generate Invite Code</a><a class='dropdown-trigger btn' href='?users'>Users</a><a class='dropdown-trigger btn' href='?login_log'>Login Log</a><a class='dropdown-trigger btn' href='/home'>Back</a>";
+    $content = ["<a class='dropdown-trigger btn' href='#' id='submit'>Generate Invite Code</a><a class='dropdown-trigger btn' href='?users'>Users</a><a class='dropdown-trigger btn' href='?login_log'>Login Log</a><a class='dropdown-trigger btn' href='/home'>Back</a>"];
     $back_button = false;
     $title = 'Admin Panel';
 }
 
 if ($back_button) {
-    $content = $content . "<a class='dropdown-trigger btn' href='/admin'>Back</a>";
+    array_push($content,"<a class='dropdown-trigger btn' href='/admin'>Back</a>");
 }
 
 ?>
@@ -55,7 +53,7 @@ if ($back_button) {
         <p class="title"><?= $title ?></p>
         <input type="hidden" name="CSRFtoken" value="<?= csrf_gen() ?>"/>
         <div class="inline">
-            <?= $content ?>
+            <?php foreach($content as $row) {echo $row;} ?>
         </div>
     </form>
 </div>
