82182c52191ea6b47998dc400fe764c3f801cb40
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-20 21:08:43 +0200

    e

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	<div class="wrapper">
 		<form class="login" method="post">
 			<p class="title">Log in</p>
-			<input placeholder="Username/Leerlingnummer" type="text" name="user_name" autocomplete="off" class="text" id="username" autofocus> <i class="fa fa-user"></i>
+			<input placeholder="Username" type="text" name="user_name" autocomplete="off" class="text" id="username" autofocus> <i class="fa fa-user"></i>
 			<input placeholder="Password" type="password" name="user_password" autocomplete="off" class="text" id="password"> <i class="fa fa-key"></i>
 			<input type="hidden" name="CSRFtoken" value="<?= csrf_gen(); ?>" />
 			<button id="submit"><i class="spinner"></i> <span class="state">Log in</span></button>
