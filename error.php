04223015bbba85732b2e0545dacc90ff15cee3ec
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-20 20:46:04 +0200

    e

--- error.php
+++ error.php
@@ -1,17 +1,1 @@
-<?php require_once($_SERVER['DOCUMENT_ROOT'] . "/functions.php"); $error = clean_data($_GET['e']);?>
-<!DOCTYPE html>
-
-<html lang="en">
-
-<?php head('Error - ' . $error); ?>
-
-<body>
-    <div class="wrapper">
-       <div class="login pd-20">
-            <p class="title"><?= 'Error - ' . $error ?></p>
-            <a href="home">Home</a>
-        </div>
-    </div>
-</body>
-
-</html>
+<?php header('Location: /home'); ?>
