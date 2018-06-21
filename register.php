309ce0e7e5b2c54505db818863cf2fc8873dde82
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-21 20:36:30 +0200

    e

--- register.php
+++ register.php
@@ -7,14 +7,9 @@ if (isset($_GET['reset'])) {
 
 if ($_SESSION['auth_code_valid']) {
     $type = 'register';
-    $title = 'Register';
-    $button_text = 'Submit';
-    $type = 'register';
     $content = ['<div class="input-field"><label for="username">Username</label><input type="text" name="user_name" class="text validate" id="username" autocomplete="off" autofocus></div><div class="input-field"><label for="password">Password</label><input type="password" name="user_password" class="text validate" id="password" autocomplete="off"></div>'];
 } else {
     $type = 'invite_code';
-    $title = 'Invite Code';
-    $button_text = 'Check Invite Code';
     $content = ['<div class="input-field"><label for="auth_code">Invite Code</label><input type="text" name="auth_code" class="text validate" id="auth_code" autocomplete="off" value="' . clean_data($_GET['auth_code']) . ' autofocus></div>'];
 }
 
@@ -25,7 +20,7 @@ if ($_SESSION['auth_code_valid']) {
 
 <html lang="en">
 
-<?php head($title); ?>
+<?php head('Register'); ?>
 
 <body>
 <div class="wrapper">
@@ -34,7 +29,7 @@ if ($_SESSION['auth_code_valid']) {
         <input type="hidden" name="type" value="<?= $type ?>"/>
         <p class="title"><?= $title ?></p>
         <?php foreach ($content as $row) {echo $row;} ?>
-        <button id="submit"><i class="spinner"></i> <span class="state"><?= $button_text ?></span></button>
+        <button id="submit"><i class="spinner"></i> <span class="state">Submit</span></button>
     </form>
 </div>
 <?php footer('register'); ?>
