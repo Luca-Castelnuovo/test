d51aec63bfc77f3717688bcf5f4b2f773ff38300
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-21 19:52:47 +0200

    e

--- index.php
+++ index.php
@@ -12,10 +12,17 @@ if (isset($_GET['logout'])) {
     <div class="wrapper">
         <form class="login" method="post">
             <p class="title">Log in</p>
+
+            <div class="input-field col s6">
             <label for="username">Username</label>
             <input type="text" name="user_name" autocomplete="off" class="text" id="username" autofocus>
+            </div>
+
+            <div class="input-field col s6">
             <label for="password">Password</label>
             <input type="password" name="user_password" autocomplete="off" class="text" id="password">
+            </div>
+
             <input type="hidden" name="CSRFtoken" value="<?= csrf_gen(); ?>" />
             <button id="submit"><i class="spinner"></i> <span class="state">Log in</span></button>
         </form>
