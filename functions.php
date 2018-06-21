95d2f42ec3cb97961ff8b896d15468d6f73ce8e1
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-21 10:54:56 +0200

    updated footer functions

--- functions.php
+++ functions.php
@@ -222,9 +222,8 @@ function head($title)
     </head>';
 }
 
-function footer($specific_js)
+function footer($specific_js = false)
 {
-    echo '<script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
-	<script src="/js/' . $specific_js . '.min.js"></script>';
+    echo '<script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script><script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>';
+    if($specific_js) {echo "<script src= '/js/{$specific_js}.min.js'></script>";}
 }
