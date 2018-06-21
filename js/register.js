a0e61c1404a0cb026b2b41707519c7fa59e4f879
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-21 20:39:49 +0200

    e

--- js/register.js
+++ js/register.js
@@ -8,11 +8,17 @@ $('#submit').click(function () {
 
     var success_response = '';
     var error_response = '';
+    var loading_message = '';
+    var url_respone_success = '';
+    var url_respone_error = '';
 
     if (register_type.val() == 'invite_code') {
         if (auth_code.val() == '') {return false;}
         success_response = 'Invite Code Valid!';
         error_response = 'Invalid Invite Code!';
+        loading_message = 'Checking Invite Code';
+        url_respone_success = '/register';
+        url_respone_error = '/register?reset';
     }
 
     if (register_type.val() == 'register') {
@@ -20,6 +26,9 @@ $('#submit').click(function () {
         if (password.val() == '') {return false;}
         success_response = 'Account Created!';
         error_response = 'Username is taken. Please choose another!';
+        loading_message = 'Registering User';
+        url_respone_success = '/';
+        url_respone_error = '/register';
     }
 
     var data = 'user_name=' + username.val() + '&user_password=' + password.val() + '&auth_code=' + auth_code.val() + '&CSRFtoken=' + CSRFtoken.val() + '&type=register&register_type=' + register_type.val();
@@ -29,7 +38,7 @@ $('#submit').click(function () {
     var $this = $('.login'),
         $state = $this.find('button > .state');
     $this.addClass('loading');
-    $state.html('Registering User');
+    $state.html(loading_message);
 
 
     $.ajax({
@@ -44,13 +53,13 @@ $('#submit').click(function () {
                 $this.addClass('ok');
                 $state.html(success_response);
                 setTimeout(function () {
-                    window.location.replace("/");
+                    window.location.replace(url_respone_success);
                 }, 1000)
             } else {
                 $this.addClass('error');
                 $state.html(error_response);
                 setTimeout(function () {
-                    window.location.replace("/register");
+                    window.location.replace(url_respone_error);
                 }, 1000)
             }
             ;
