f09e8c23021d87c4c0af99b9914ee341bd142617
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-21 19:50:18 +0200

    e

--- index.php
+++ index.php
@@ -9,18 +9,18 @@ if (isset($_GET['logout'])) {
 <?php head('Log In'); ?>
 
 <body>
-<div class="wrapper">
-    <form class="login" method="post">
-        <p class="title">Log in</p>
-        <input placeholder="Username" type="text" name="user_name" autocomplete="off" class="text" id="username"
-               autofocus>
-        <input placeholder="Password" type="password" name="user_password" autocomplete="off" class="text"
-               id="password">
-        <input type="hidden" name="CSRFtoken" value="<?= csrf_gen(); ?>"/>
-        <button id="submit"><i class="spinner"></i> <span class="state">Log in</span></button>
-    </form>
-</div>
-<?php footer('login'); ?>
+    <div class="wrapper">
+        <form class="login" method="post">
+            <p class="title">Log in</p>
+            <label for="username">Username</label>
+            <input placeholder="Username" type="text" name="user_name" autocomplete="off" class="text" id="username" autofocus>
+            <label for="password">Password</label>
+            <input placeholder="Password" type="password" name="user_password" autocomplete="off" class="text" id="password">
+            <input type="hidden" name="CSRFtoken" value="<?= csrf_gen(); ?>" />
+            <button id="submit"><i class="spinner"></i> <span class="state">Log in</span></button>
+        </form>
+    </div>
+    <?php footer('login'); ?>
 </body>
 
 </html>
