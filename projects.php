b36a54dc245df6d850da22d726be9effef6d76fd
Brian van Meggelen <bvmeggelen@gmail.com>
2018-06-21 10:51:52 +0200

    updated footer for $(this)

--- projects.php
+++ projects.php
@@ -41,8 +41,7 @@ switch ($_GET['type']) {
         <button id="submit"><span class="state"><?= $button_text ?></span></button>
     </form>
 </div>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-<script src="/js/projects.min.js"></script>
+<?php footer('projects'); ?>
 </body>
 
 </html>
