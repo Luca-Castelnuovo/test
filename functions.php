c87f25408ca36ce32a2c78986bb930564f606767
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-21 10:46:31 +0200

    added materilize css

--- functions.php
+++ functions.php
@@ -218,5 +218,6 @@ function head($title)
 		<link rel="stylesheet" type="text/css" href="/css/style.min.css">
 		<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 		<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans:400,700">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
     </head>';
 }
