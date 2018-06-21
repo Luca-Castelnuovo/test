2f534ac077333b6101b04fb946420dd5b7e98c6b
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-21 19:46:05 +0200

    e

---
+++ test.php
@@ -0,0 +1,31 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'] . "/functions.php");
+if (isset($_GET['logout'])) {
+    logout();
+} ?>
+<!DOCTYPE html>
+
+<html lang="en">
+
+<?php head('Log In'); ?>
+
+<body>
+<div class="row">
+    <form class="col s12">
+        <div class="row">
+            <div class="input-field col s6">
+                <i class="material-icons prefix">account_circle</i>
+                <input id="password" type="text" class="validate text" name="user_name" autocomplete="off">
+                <label for="password">Username</label>
+            </div>
+            <div class="input-field col s6">
+                  <i class="material-icons prefix">lock</i>
+                <input id="username" type="password" class="validate text" name="user_password" autocomplete="off">
+                <label for="password">Password</label>
+            </div>
+        </div>
+    </form>
+  </div>
+<?php footer('login'); ?>
+</body>
+
+</html>
