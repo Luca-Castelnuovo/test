bac4581e79ba30bd65dc47d0a20ea84be9d5d4de
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-21 10:49:43 +0200

    updeated header and added footeer

--- functions.php
+++ functions.php
@@ -221,3 +221,10 @@ function head($title)
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
     </head>';
 }
+
+function footer($specific_js)
+{
+    echo '<script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+	<script src="/js/' . $specific_js . '.min.js"></script>';
+}
