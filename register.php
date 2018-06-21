9853dbd3391b97a718ab3e9488b139e2973f1c3a
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-06-21 20:27:09 +0200

    e

--- register.php
+++ register.php
@@ -10,12 +10,12 @@ if ($_SESSION['auth_code_valid']) {
     $title = 'Register';
     $button_text = 'Submit';
     $type = 'register';
-    $content = ['<input placeholder="Username" type="text" name="user_name" autocomplete="off" class="text" id="username" autofocus>', '<input placeholder="Password" type="password" name="user_password" autocomplete="off" class="text" id="password">'];
+    $content = ['<div class="input-field"><label for="username">Username</label><input type="text" name="user_name" class="text validate" id="username" autocomplete="off" autofocus></div><div class="input-field"><label for="password">Password</label><input type="password" name="user_password" class="text validate" id="password" autocomplete="off"></div>'];
 } else {
     $js = 'auth';
     $title = 'Invite Code';
     $button_text = 'Check Invite Code';
-    $content = ['<input placeholder="Invite Code" type="text" name="auth_code" id="auth_code" autocomplete="off" class="text" autofocus value="' . clean_data($_GET['auth_code']) . '">'];
+    $content = ['<div class="input-field"><label for="auth_code">Invite Code</label><input type="text" name="auth_code" class="text validate" id="auth_code" autocomplete="off" value="' . clean_data($_GET['auth_code']) . ' autofocus></div>'];
 }
 
 ?>
