a7ae407dc14a23c322902a3c7739914628759553
Brian van Meggelen <bvmeggelen@gmail.com>
2018-06-23 09:18:11 +0200

    added link to font Montserrat

--- functions.php
+++ functions.php
@@ -216,7 +216,7 @@ function head($title)
     <meta content="width=device-width,initial-scale=1,shrink-to-fit=no" name=viewport>
     <link href=https://lucacastelnuovo.nl/images/favicon.ico rel="shortcut icon">
     <link rel="stylesheet" type="text/css" href="/css/style.min.css">
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans:400,700">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Montserrat|Open+Sans:400,700">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     </head>';
