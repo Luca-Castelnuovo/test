184133405239eb34e2b81cb44be1c0b5b399b2b2
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-07-19 16:54:21 +0200

    Small bugfixes

--- functions.php
+++ functions.php
@@ -236,9 +236,9 @@ function footer($specific_js = false)
     echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdn.lucacastelnuovo.nl/js/vanilla/loader.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
-    <script src="https://cdn.lucacastelnuovo.nl/php/background.php?background=7"></script>
-    <script>setTimeout(stopLoading, 100);</script>';
+    <script src="https://cdn.lucacastelnuovo.nl/php/background.php?background=7"></script>';
     if ($specific_js) {
         echo "<script src= '/js/{$specific_js}.js'></script>";
     }
+    echo '<script>setTimeout(stopLoading, 100);</script>';
 }
