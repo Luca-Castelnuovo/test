c8b7a3f301e2b2c050a4ff079c107b4660ca18ca
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-12-27 16:10:45 +0100

    update

--- old_files.php
+++ old_files.php
@@ -1,29 +1,8 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'] . "/functions.php");
-login();
 
-$id = clean_data($_GET['id']);
-$project_id = clean_data($_GET['project_id']);
-
-$show_button = true;
 switch ($_GET['type']) {
-    case 'add':
-        $title = 'Add File';
-        $content = ['<div class="input-field"><label for="file_name">File Name</label><input type="text" name="file_name" class="text validate" id="file_name" autocomplete="off" required autofocus></div>', '<p>Please choose a file type</p>', '<p><label><input name="file_lang" type="radio" value="html" checked> <span>HTML</span></label></p>', '<p><label><input name="file_lang" type="radio" value="css"> <span>CSS</span></label></p>', '<p><label><input name="file_lang" type="radio" value="js"> <span>JavaScript</span></label></p>'];
-        $button_text = 'Create File';
-        break;
 
     case 'edit':
-        $title = 'Edit File';
-        $files = sql("SELECT file FROM files WHERE id='{$id}'AND owner_id='{$_SESSION['user_id']}'", true);
-        $projects = sql("SELECT project_name FROM projects WHERE id='{$project_id}'AND owner_id='{$_SESSION['user_id']}'", true);
-        $project_name = $projects['project_name'];
-        $file = "users/{$_SESSION['user_name']}/{$project_name}/{$files['file']}";
-        $file_open = fopen($file, "r");
-        $file_content = fread($file_open, filesize($file));
-        fclose($file);
-        $show_preview = null;
-        $mode = substr($files['file'], strrpos($files['file'], '.') + 1);
         switch ($mode) {
         case 'html':
             $show_preview= '<iframe id=preview></iframe>';
@@ -36,18 +15,6 @@ switch ($_GET['type']) {
             $mode_js = '<script src="https://cdn.lucacastelnuovo.nl/test.lucacastelnuovo.nl/js/codemirror/javascript.js"></script><script src="https://cdn.lucacastelnuovo.nl/test.lucacastelnuovo.nl/js/javascript.js"></script><style>.CodeMirror{float:none;width:100%;}</style>';
             break;
         }
-        $content = ['<textarea name="textarea" class="secret">' . $file_content . '</textarea>', '<div style="display: flex;"><textarea name="file_content" class="text" id="code" placeholder="Enter your code here..."></textarea>', $show_preview, '</div>'];
-        $button_text = 'Save File';
-        break;
-
-    case 'delete':
-        $title = 'Delete File';
-        $content = ['<label for="delete">To confirm type `delete`</label>', '<input type="text" name="file_delete" class="text validate" id="delete" autocomplete="off" required autofocus>'];
-        $button_text = 'Submit';
-        break;
-
-    default:
-        logout();
         break;
 }
 
