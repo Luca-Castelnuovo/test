7508816e713e1f7bd2fc8b87216ab8cf6437f0a8
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-12-27 10:55:32 +0100

    created project add system

---
+++ project.php
@@ -0,0 +1,56 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $project_name = check_data($_POST['name'], true, 'Project name', true);
+
+    $project_name = strtolower($project_name);
+    $project_name = str_replace(' ', '_', $project_name);
+
+    $existing_project = sql_select('projects', 'id', "owner_id='{$user_id}'  AND name='{$project_name}'", false);
+    if ($existing_project->num_rows > 0) {
+        redirect('/project', 'You already have a project with this name.');
+    }
+
+    sql_insert('projects', [
+        'owner_id' => $_SESSION['id'],
+        'name' => $project_name
+    ]);
+
+    mkdir("users/{$_SESSION['username']}/{$project_name}", 0770);
+
+    redirect('/home', 'Project created');
+}
+
+page_header('Create Project');
+
+?>
+
+<style>
+    .input-field input:focus + label {
+        color: #2962FF !important;
+    }
+
+    .input-field input:focus {
+        border-bottom: 1px solid #2962FF !important;
+        box-shadow: 0 1px 0 0 #2962FF !important;
+    }
+</style>
+<div class="row">
+    <h4>Create Client</h4>
+    <form method="post">
+        <div class="row">
+            <div class="input-field col s12">
+                <label for="name">Project Name</label>
+                <input type="text" id="name" name="name" required/>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s12">
+                <input type="hidden" name="CSRFtoken" value="<?= csrf_gen() ?>"/>
+                <button class="col s12 btn waves-effect blue accent-4" type="submit">Create Project</button>
+            </div>
+        </div>
+    </form>
+</div>
+
+<?= page_footer(); ?>
--- project/add.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-//unique projectname
-//strtolower
-//trim projectname
-//change space in the middle to underscore (strreplace)
