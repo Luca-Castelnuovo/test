1a8bf108bd58177dbe7b2cbcf4451e205d89da3f
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-12-27 14:28:26 +0100

    update

--- files/edit.php
+++ files/edit.php
@@ -5,25 +5,26 @@ require($_SERVER['DOCUMENT_ROOT'] . '/includes/init.php');
 $project_id = check_data($_GET['project_id'], true, 'Project ID', true, '/home');
 $file_id = check_data($_GET['file_id'], true, 'File ID', true, '/home?project_id' . $project_id);
 
-$file = sql_select('files', 'name', "owner_id='{$_SESSION['id']}'  AND id='{$file_id}' AND project_id='{$project_id}'", false);
-if ($file->num_rows != 1) {
+$file_sql = sql_select('files', 'name', "owner_id='{$_SESSION['id']}'  AND id='{$file_id}' AND project_id='{$project_id}'", false);
+if ($file_sql->num_rows != 1) {
     redirect('/home?project_id=' . $project_id, 'File doen\'t exist');
 } else {
-    $file_assoc = $file->fetch_assoc();
+    $file_assoc = $file_sql->fetch_assoc();
 }
 
 $project = sql_select('projects', 'name', "owner_id='{$_SESSION['id']}'  AND id='{$project_id}'", true);
-$file = fopen("../users/{$_SESSION['username']}/{$project['name']}/{$file_assoc['name']}", "r+");
+$file = "../users/{$_SESSION['username']}/{$project['name']}/{$file_assoc['name']}";
+$file_open = fopen($file, "r+");
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     csrf_val($_POST['CSRFtoken'], '/project');
     $project = sql_select('projects', 'name', "owner_id='{$_SESSION['id']}'  AND id='{$project_id}'", true);
-    fwrite($file, $_POST['content'] . PHP_EOL);
-    fclose($file);
+    fwrite($file_open, $_POST['content'] . PHP_EOL);
+    fclose($file_open);
 
     redirect('/home?project_id=' . $project_id, 'File updated');
 } else {
-    fclose($file);
+    fclose($file_open);
 }
 
 page_header('Edit File');
@@ -35,7 +36,7 @@ page_header('Edit File');
     <form method="post" action="?project_id=<?= $project_id ?>&file_id=<?= $file_id ?>">
         <div class="row">
             <div class="col s12">
-                <textarea name="content" rows="8" cols="80"><?= $file ?></textarea>
+                <textarea name="content" rows="8" cols="80"><?= fread($file_open, filesize($file)); ?></textarea>
             </div>
         </div>
         <div class="row">
