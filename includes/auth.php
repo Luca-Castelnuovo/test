4706caf31241b9dbeffb46e44cb583ccdc14e418
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-01-12 13:03:50 +0100

    removed too many api rewuest

--- includes/auth.php
+++ includes/auth.php
@@ -23,12 +23,6 @@ function login($access_token)
 
 function loggedin()
 {
-    try {
-        api_get_token($_SESSION['access_token']);
-    } catch (Exception $error) {
-        redirect('/?reset', 'Please login');
-    }
-
     if ((!$_SESSION['logged_in']) || ($_SESSION['ip'] != $_SERVER['REMOTE_ADDR']) || (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1800))) {
         redirect("/?reset", 'Please login');
     } else {
