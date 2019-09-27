a4f7903daf745821243b549f997390463528068b
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-09-27 09:35:21 +0200

    removed analytics

--- includes/template.php
+++ includes/template.php
@@ -25,20 +25,6 @@ function page_header($title = 'Unknown')
         <!-- Styles -->
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-
-        <!-- Analytics -->
-        <script type="text/javascript">
-            var _paq = window._paq || [];
-            _paq.push(['trackPageView']);
-            _paq.push(['enableLinkTracking']);
-            (function() {
-                var u="//analytics.lucacastelnuovo.nl/";
-                _paq.push(['setTrackerUrl', u+'matomo.php']);
-                _paq.push(['setSiteId', '13']);
-                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-                g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-            })();
-        </script>
     </head>
 
     <body>
