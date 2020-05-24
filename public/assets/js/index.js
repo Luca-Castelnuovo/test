6c880dc2407097802c12f95b3afc3816023edaf1
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-05-24 14:36:38 +0200

    updates

---
+++ public/assets/js/index.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function(){
+    M.Sidenav.init(document.querySelectorAll('.sidenav'), {edge:"right"});
+    M.Modal.init(document.querySelectorAll('.modal'), {});
+});
