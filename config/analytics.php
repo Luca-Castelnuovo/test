18e330f1c4da6d6e3ab09cd2037b830ea72b47ac
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-06-21 21:32:54 +0200

    Update Analytics

--- config/analytics.php
+++ config/analytics.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    'enabled' => true,
-    'domainId' => '0cfb7d34-c3b1-492f-8552-129dab201b09',
-    'options' => '{ "localhost": false, "detailed": true }'
+    'enabled' => false,
+    'domainId' => ''
 ];
